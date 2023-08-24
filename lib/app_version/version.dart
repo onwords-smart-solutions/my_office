0a1b73e116983da35432e748785eb9e1a5db873e
Deva <devendiran@onwords.in>
2023-08-24 12:06:56 +0530

    Updated invoice

--- lib/app_version/version.dart
+++ lib/app_version/version.dart
@@ -1,8 +1,8 @@
 class AppConstants{
 
   //PUBSPEC AND DEFAULT VERSION FOR ANDROID
-  static String displayVersion = '1.1.45';
-  static String pubVersion = '1.1.45+65';
+  static String displayVersion = '1.1.46';
+  static String pubVersion = '1.1.46+66';
 
   //PUBSPEC AND DEFAULT VERSION FOR IOS
   static String iosDisplayVersion = '1.1.20';
